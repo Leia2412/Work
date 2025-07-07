4ccbc1dec8849c9b83d59e98855e2d2a951afef8
Leia2412 <princessleiaaj@gmail.com>
2025-07-07 10:12:47 -0400

    Add files via upload

---
+++ Jules.py
@@ -0,0 +1,8 @@
+print("hello everyone I am Jules")
+name= input ("How are you doing?")
+print(f"I a doing very good{name}")
+feelings = input ("how are you feeling Today?")
+if "good" in feelings.lower() or "great" in feelings.lower():
+   print ("That's good to hear")
+else:
+   print("That's very sad")
