be5e56ac32593680e5c8f63d9229129e8c2e112a
Leia2412 <princessleiaaj@gmail.com>
2025-07-11 11:52:03 -0400

    Add files via upload

---
+++ Congratulations message.py
@@ -0,0 +1,13 @@
+# Ram's message in lowercase
+message = "congratulations"
+
+# Convert to uppercase
+uppercase_message = message.upper()
+
+# Display the final result
+print("Ram's congratulatory message is:You did great!! I would like to congratulate you and I know you will be even better next year with more pratice and alot more courage!!")
+print(uppercase_message)
+
+
+
+
