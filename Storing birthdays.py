9806645b02843d37858228c35d865393068b487d
Leia2412 <princessleiaaj@gmail.com>
2025-07-11 11:25:00 -0400

    Add files via upload

---
+++ Storing birthdays.py
@@ -0,0 +1,15 @@
+# Store birthdays of Raj's five friends
+friend1_birthday = "January 5"
+friend2_birthday = "March 12"
+friend3_birthday = "June 30"
+friend4_birthday = "October 18"
+friend5_birthday = "December 25"
+
+# Print out the birthdays
+print("Raj's friends' birthdays are:")
+print("Friend 1:", friend1_birthday)
+print("Friend 2:", friend2_birthday)
+print("Friend 3:", friend3_birthday)
+print("Friend 4:", friend4_birthday)
+print("Friend 5:", friend5_birthday)
+
